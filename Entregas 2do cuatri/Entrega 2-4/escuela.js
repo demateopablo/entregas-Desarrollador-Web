4268219fe3909e1666175a11aa44f935f8e6f285
demateopablo <pablodem32@gmail.com>
2025-03-30 16:04:09 -0300

    Avances entrega

---
+++ Entregas 2do cuatri/Entrega 2-4/escuela.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Escuela = void 0;
+var Escuela = /** @class */ (function () {
+    function Escuela(nombre, docentes) {
+        this.nombre = nombre;
+        this.docentes = docentes;
+    }
+    Escuela.prototype.getDocente = function (posicion) {
+        return this.docentes[posicion];
+    };
+    Escuela.prototype.getDocentes = function () {
+        var infoDocentes = "";
+        this.docentes.forEach(function (docente) {
+            infoDocentes += "".concat(docente.getNombreCompleto(), "\n");
+        });
+        return infoDocentes;
+    };
+    Escuela.prototype.getDocentesYAlumnos = function () {
+        var infoDocentesYAlumnos = "";
+        this.docentes.forEach(function (docente) {
+            infoDocentesYAlumnos += "Docente:\t".concat(docente.getNombreCompleto(), "\nAlumnos:\n");
+            infoDocentesYAlumnos += docente.getAlumnos();
+        });
+        return infoDocentesYAlumnos;
+    };
+    return Escuela;
+}());
+exports.Escuela = Escuela;
---
+++ Entregas 2do cuatri/Entrega 2-4/index.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var escuela_1 = require("./escuela");
+var docente_1 = require("./docente");
+var alumno_1 = require("./alumno");
+var alumno1 = new alumno_1.Alumno('Juan Perez', 8);
+var alumno2 = new alumno_1.Alumno('Lucio Gomez', 4);
+var alumno3 = new alumno_1.Alumno('Julian Alvarez', 9);
+var alumno4 = new alumno_1.Alumno('Pedro Arias', 1);
+var docente1 = new docente_1.Docente('Lalo Landa', [alumno1, alumno2, alumno3, alumno4]);
+var alumno5 = new alumno_1.Alumno('Maria Hernandez', 3);
+var alumno6 = new alumno_1.Alumno('Noah Luna', 7);
+var alumno7 = new alumno_1.Alumno('Mariana Estevez', 8);
+var alumno8 = new alumno_1.Alumno('Pablo Demateo', 10);
+var docente2 = new docente_1.Docente('Julia Fernandez', [alumno5, alumno6, alumno7, alumno8]);
+var escuela1 = new escuela_1.Escuela('CEPIT', [docente1, docente2]);
+/* console.log(`\nDocente:\t${escuela1.getDocente(0).getNombreCompleto()}`); */
+/* console.log(escuela1.getDocente(0).getAlumno(0).getNombreYAprobacion());
+console.log(escuela1.getDocente(0).getAlumno(1).getNombreYAprobacion());
+console.log(escuela1.getDocente(0).getAlumno(2).getNombreYAprobacion());
+console.log(escuela1.getDocente(0).getAlumno(3).getNombreYAprobacion());
+console.log(`\nDocente:\t${escuela1.getDocente(1).getNombreCompleto()}`);
+console.log(escuela1.getDocente(1).getAlumno(0).getNombreYAprobacion());
+console.log(escuela1.getDocente(1).getAlumno(1).getNombreYAprobacion());
+console.log(escuela1.getDocente(1).getAlumno(2).getNombreYAprobacion());
+console.log(escuela1.getDocente(1).getAlumno(3).getNombreYAprobacion()); */
+/* console.log(escuela1.getDocente(0).getAlumnos()); */
+console.log(escuela1.getDocentesYAlumnos());
